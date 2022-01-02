56d06d9f8ff07e0c617dbd9edcf711ca725b3585
rnjsehdgks01 <62535103+rnjsehdgks01@users.noreply.github.com>
2022-01-02 11:50:02 +0900

    eureka client 설정

--- src/main/java/com/msa/order/repository/OrderRepository.java
+++ src/main/java/com/msa/order/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.msa.order.repository;
 
-import com.msa.order.domain.Order;
+import com.msa.order.domain.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 }
